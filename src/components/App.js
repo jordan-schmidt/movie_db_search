ba3dc654dce88984e493110d05ce867c70c703a5
Jordan Schmidt <jordan@schmidt6.com>
2018-01-23 12:08:05 -0800

    adds movie db api search functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
+import fetch from 'node-fetch';
 import './App.css';
 
 import SearchBar from './search_bar'
 
+const ROOT_URL = 'https://api.themoviedb.org/3';
+const API_KEY = process.env.REACT_APP_MOVIE_DB_API_KEY;
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: [],
+      currentMovie: null
+    };
+
+    this.movieSearch('Reservoir Dogs');
+  }
+
+  movieSearch(term) {
+    let query = encodeURI(term);
+    fetch(`${ROOT_URL}/search/movie?api_key=${API_KEY}&query=${query}`)
+      .then(res => res.json())
+      .then(resJSON => {
+        this.setState({
+          movies: resJSON.results,
+          currentMovie: resJSON.results[0]
+        });
+      });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <SearchBar />
